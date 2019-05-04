ecd2d6aa139f23653029b8301527259dc99bfd25
PC <PC@192.168.1.114>
2019-05-04 20:19:31 +0200

    eldo

--- Projekt/src/projektInterfejs/BottomPanel.java
+++ Projekt/src/projektInterfejs/BottomPanel.java
@@ -7,40 +7,44 @@ import java.awt.LayoutManager;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
-import projektInterfejs.FirstEdition.SliderChangeListener;
+
 
 public class BottomPanel extends JPanel {
 
 	//bottom
 	private JPanel bottomPanel;
 	private JLabel objectLabel;
+	private JSlider slider;
+	private int minValue= 0;
+	private int maxValue=100;
+	private int startValue=0;
 	
 	public BottomPanel() {
 
 		//bottom
-				bottomPanel= new JPanel();
-				objectLabel= new JLabel("1st object");
-				
-				
-				
-				bottomPanel.add(objectLabel);
-				this.add(bottomPanel, BorderLayout.PAGE_END);	
+		bottomPanel= new JPanel();
+		objectLabel= new JLabel("1st object");
+		
+		slider= new JSlider(JSlider.HORIZONTAL, minValue, maxValue, startValue);  
+		slider.setPreferredSize(new Dimension(200, 50));						
+		slider.setMajorTickSpacing(10);											
+		slider.setMinorTickSpacing(5);
+		slider.setPaintTicks(true);
+		slider.setPaintLabels(true);
+		slider.addChangeListener(new SliderChangeListener());	
+		
+		this.add(objectLabel);
+		this.add(slider);
 	}
-
-	public BottomPanel(LayoutManager layout) {
-		super(layout);
-		// TODO Auto-generated constructor stub
-	}
-
-	public BottomPanel(boolean isDoubleBuffered) {
-		super(isDoubleBuffered);
-		// TODO Auto-generated constructor stub
-	}
-
-	public BottomPanel(LayoutManager layout, boolean isDoubleBuffered) {
-		super(layout, isDoubleBuffered);
-		// TODO Auto-generated constructor stub
+	public class SliderChangeListener implements ChangeListener {
+		@Override
+		public void stateChanged(ChangeEvent arg0) {
+			String wartosc= String.format("%d", slider.getValue());
+			repaint();
+		}		
 	}
 
 }
--- Projekt/src/projektInterfejs/Frame.java
+++ Projekt/src/projektInterfejs/Frame.java
@@ -1,45 +1,45 @@
 package projektInterfejs;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.GraphicsConfiguration;
 import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
 import javax.swing.JTextField;
 
-import projektInterfejs.FirstEdition.drawPanel;
 
 public class Frame extends JFrame {
 
-		//right
-		private JPanel rightPanel;
-		private JLabel lensLabel;
-		private String[] lenses= {"converging", "diverging"};
-		private String[] lensShape= {"arrow","triangle", "square", "pentagon", "hexagon"};
-		private JComboBox lensType;
-		private JLabel nLabel;
-		private JTextField nTextField;
-		private JComboBox objectType;
-		private JComboBox objectType2;
-		private JButton add;
-		private JTextField focalTextField;
-		private JLabel language, objectLabel1, objectLabel2, focalLabel;
-		private JButton polskiButton, englishButton, saveButton, newButton;
 		
 		//bottom
 		private JPanel bottomPanel;
 		
-		//private Polygon poly;
 		private JSlider slider;
 		private int minValue= 0;
 		private int maxValue=100;
 		private int startValue=0;
 		private JLabel objectLabel;
+		
+		
+		private JMenuBar menuBar;
+		private JMenu menu;
+		private JMenuItem menuItemW;
+		private JMenuItem menuItemM;
+		private JMenuItem menuItemC;
+		private JMenuItem menuItemY;
+		
+		
 		
 	public Frame() throws HeadlessException {
 		
@@ -47,23 +47,54 @@ public class Frame extends JFrame {
 		this.setLayout(new BorderLayout());
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		
+		//////////////////////////////////////////////////menu
+		menuBar= new JMenuBar();
+		menu= new JMenu("Background color");
+		menuItemW= new JMenuItem("White");
+		menuItemM= new JMenuItem("Magenta");
+		menuItemC= new JMenuItem("Cyan");
+		menuItemY= new JMenuItem("Yellow");
+		DrawPanel drawpanel= new DrawPanel();
+		drawpanel.setBackground(Color.white);
 		
-		
-	}
+		menuItemW.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent b) {	
+				drawpanel.setBackground(Color.WHITE);
+			}
+		});
+		menuItemM.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent b) {				
+				drawpanel.setBackground(Color.magenta);
+			}
+		});
+		menuItemC.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent b) {
+				drawpanel.setBackground(Color.cyan);
+			}
+		});
+		menuItemY.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent b) {				
+				drawpanel.setBackground(Color.yellow);
+			}
+		});
+	
+		menu.add(menuItemW);
+		menu.add(menuItemM);
+		menu.add(menuItemC);
+		menu.add(menuItemY);
+		menuBar.add(menu);
+		this.setJMenuBar(menuBar);
+		////////////////////////////////////////////////////end of menu
 
-	public Frame(GraphicsConfiguration arg0) {
-		super(arg0);
-		// TODO Auto-generated constructor stub
-	}
+	    RightPanel RPanel = new RightPanel();
+	    BottomPanel BPanel = new BottomPanel();
 
-	public Frame(String arg0) throws HeadlessException {
-		super(arg0);
-		// TODO Auto-generated constructor stub
+	    this.add(drawpanel, BorderLayout.CENTER);	    
+	    this.add(RPanel, BorderLayout.LINE_END);
+	    this.add(BPanel, BorderLayout.PAGE_END);	    
 	}
-
-	public Frame(String arg0, GraphicsConfiguration arg1) {
-		super(arg0, arg1);
-		// TODO Auto-generated constructor stub
-	}
-
 }
